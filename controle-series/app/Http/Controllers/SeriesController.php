e5920b4f284fc1391a9343832b1dbde8ada94bee
matheus.andrade <estagiario.sistemas@portodesantos.com.br>
2019-11-08 16:34:44 -0300

    complete aula04  'laravel01'

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -2,20 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Serie;
+use Illuminate\Http\Request;
+
 class SeriesController extends Controller
 {
     public function index(){
-        $series = [
-            'Breaking Bad',
-            'Marco Polo',
-            'Mr Robot',
-            'Dark'
-        ];
+        $series = serie::all();
 
         return view('series.index', compact('series'));
     }
 
     public function create(){
         return view('series.create');
+    }
+
+    public function store(Request $request){
+        $serie = Serie::create($request->all());
+
+        echo "Série com id {$serie->id} criada: {$serie->nome}";
     }
 }
--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -8,9 +8,9 @@
 @section('conteudo')
     <a href="/series/criar" class="btn btn-dark mb-3">Adicionar</a>
     <ul class="list-group">
-        <?php foreach ($series as $serie): ?>
-        <li class="list-group-item"><?=$serie?></li>
-        <?php endforeach;?>
+        @foreach ($series as $serie)
+            <li class="list-group-item">{{ $serie->nome }}</li>
+        @endforeach
     </ul>
 @endsection
 

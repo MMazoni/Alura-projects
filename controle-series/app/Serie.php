a46b3e3489acb614a5c919adfcdab604e717ce88
matheus.andrade <estagiario.sistemas@portodesantos.com.br>
2019-11-11 16:55:12 -0300

    complete aula01 'laravel02'

--- controle-series/app/Serie.php
+++ controle-series/app/Serie.php
@@ -2,10 +2,23 @@
 
 namespace App;
 
+use App\Episodio;
+use App\Temporada;
 use Illuminate\Database\Eloquent\Model;
 
 class Serie extends Model
 {
     public $timestamps = false;
     protected $fillable = ['nome'];
+
+
+    public function temporadas()
+    {
+        return $this->hasMany(Temporada::class);
+    }
+
+    public function episodios()
+    {
+        return $this->hasMany(Episodio::class);
+    }
 }

9dd605ffac08acc3d8ebe59f5abbc82b5007d752
Matheus Mazoni de Andrade <mmazoni.andrade@gmail.com>
2020-02-12 09:31:14 -0300

    complete aula04 laravel-02

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,6 +7,9 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
     }
     
     adiciona(event) {
@@ -15,9 +18,9 @@ class NegociacaoController {
         
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._listaNegociacoes.negociacoes.push(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
 
-        console.log(this._listaNegociacoes.negociacoes);
     }
 
     _limpaFormulario() {

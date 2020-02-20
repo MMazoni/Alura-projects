a6b3c2ba7f976f8faf1a40d5a78acb111b2c5953
MMazoni <mmazoni.andrade@gmail.com>
2020-02-20 13:16:05 -0300

    início aula03 php+mysql

--- phpweb-master/src/Artigo.php
+++ phpweb-master/src/Artigo.php
@@ -18,6 +18,12 @@ class Artigo
 
     }
 
+    public function adicionar(string $titulo, string $conteudo): void
+    {
+        $insereArtigo = $this->mysql->query('INSERT INTO artigos (titulo, conteudo) VALUES (?, ?)');
+        $insereArtigo->bind_param('ss',$titulo, $conteudo);
+    }
+
     public function encontrarPorId(string $id): array
     {
         $selecionaArtigo = $this->mysql->prepare('SELECT id, titulo, conteudo FROM artigos WHERE id = ?');
